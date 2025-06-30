e196fd9ffc00c43715dd9a361db7e76e8c7b7b45
Marisol Castillo <36350945@fi.unju.edu.ar>
2025-06-29 21:18:30 -0300

    Se modifico el archivo index.js, corrigiendo una lineas que no correspondian

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,9 +7,9 @@ import authFluentRoutes from '../modules/auth/route/auth.fluent.routes.js';
 import userFluentRoutes from '../modules/user/route/user.fluent.routes.js';
 import personaFluentRoutes from '../modules/persona/route/persona.fluent.routes.js';
 import profesorRoutes from '../modules/profesor/routes/profesor-routes.js';
-import torneoRoutes from '../modules/torneo/routes/torneo.routes.js'import alumnoRoutes from '../modules/alumno/route/alumno.route.js';
+import torneoRoutes from '../modules/torneo/routes/torneo.routes.js';
+import alumnoRoutes from '../modules/alumno/route/alumno.route.js';
 import alumnoCategoriaRoutes from '../modules/alumno_categoria/route/alumno_categoria.route.js';
-import categoriaRoutes from '../modules/categoria/route/categoria.route.js';
 import cuotaRoutes from '../modules/cuota/route/cuota.route.js';
 
 // Importar rutas de desarrollo
@@ -34,7 +34,6 @@ router.use('/profesores', profesorRoutes);
 router.use('/torneos', torneoRoutes);
 router.use('/alumnos', alumnoRoutes);
 router.use('/alumno-categorias', alumnoCategoriaRoutes);
-router.use('/categorias', categoriaRoutes);
 router.use('/cuotas', cuotaRoutes);
 
 // Rutas de desarrollo (solo disponibles en NODE_ENV=development)
