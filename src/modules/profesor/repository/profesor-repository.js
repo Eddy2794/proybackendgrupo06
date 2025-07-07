17aac723c328b0e20ac2816cfa97d317e360e6b1
mili-08 <milijmartinez@gmail.com>
2025-07-07 10:12:57 -0300

    Se mejoro el back para adapatarlo a las validaciones del front

--- src/modules/profesor/repository/profesor-repository.js
+++ src/modules/profesor/repository/profesor-repository.js
@@ -1,4 +1,7 @@
+import * as personaRepository from '../../persona/repository/persona.repository.js';
 import Profesor from '../model/profesor-model.js';
+import * as profesorCategoriaService from '../../profesor-categoria/service/profesor-categoria.service.js';
+import * as personaService from '../../persona/service/persona.service.js';
 
 export const create = async(data) => {
     const profesor = new Profesor(data);
@@ -16,7 +19,7 @@ export const findById = async (id) => {
 }
 
 export const findAll = async () => {
-    return await Profesor.find()
+    return await Profesor.find({ activo_laboral: true })
         .populate('persona');
 }
 
@@ -27,3 +30,23 @@ export const updateById = async(id, profesorData) => {
 export const deleteById = async(id) => {
     return await Profesor.findByIdAndDelete(id);
 }
+export const deleteProfesor = async (id) => {
+    // Desactivar relaciones en profesor-categoria usando el service
+    await profesorCategoriaService.desactivarPorProfesor(id);
+
+    // Buscar el profesor para obtener el id de persona
+    const profesor = await findById(id);
+    if (!profesor) {
+        throw new Error('Error al eliminar el profesor');
+    }
+
+    // Eliminación lógica: poner activo_laboral en false
+    await updateById(id, { activo_laboral: false });
+
+    // Eliminación lógica de la persona asociada
+    if (profesor.persona && profesor.persona._id) {
+        await personaService.softDeletePersona(profesor.persona._id);
+    }
+
+    return { ...profesor.toObject(), activo_laboral: false };
+};
